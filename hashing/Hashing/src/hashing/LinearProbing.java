86ef20ac9b651e5d74c05079445cc129569a06d9
Francesco Stefan <108467822+Francescostefan1998@users.noreply.github.com>
2024-05-19 16:50:41 +0200

    LinearProbing with all his method

---
+++ hashing/Hashing/src/hashing/LinearProbing.java
@@ -0,0 +1,73 @@
+package hashing;
+import java.util.*;
+
+public class LinearProbing {
+
+	String[] hashTable;
+	int usedCellNumber;
+	
+	public LinearProbing(int size) {
+		hashTable = new String[size];
+		usedCellNumber = 0;
+	}
+	
+	public int modASCIIHashFunction(String word, int M){
+		char ch[];
+		ch = word.toCharArray();
+		int i, sum;
+		for(sum = 0, i = 0; i < word.length(); i++) {
+			sum = sum + ch[i];			
+		}
+		return sum % M;
+	}
+	
+	public double getLoadFactor() {
+		double loadFactor = usedCellNumber * 1.0/hashTable.length;
+		return loadFactor;
+	}
+	
+	public void rehashKeys(String word) {
+		ArrayList<String> data = new ArrayList<String>();
+		for(String s: hashTable) {
+			data.add(s);
+		}
+		data.add(word);
+		hashTable = new String[hashTable.length * 2];
+		for(String s: data) {
+			insert(s);
+		}
+	}
+	
+	public void insert(String word) {
+		double loadFactor = getLoadFactor();
+		if(loadFactor >= 0.75) {
+			rehashKeys(word);
+		} else {
+			int index = modASCIIHashFunction(word, hashTable.length);
+			for(int i = index; i < index +hashTable.length; i++) {
+				int newIndex = i %  hashTable.length;
+				if(hashTable[newIndex] == null) {
+					hashTable[newIndex] = word;
+					System.out.println("The "+ word + " successfully inserted at location: " + newIndex);
+					break;
+				} else {
+					System.out.println(newIndex + " is already occupied. Trying next empty cell");
+				}
+			}
+		}
+		usedCellNumber++;
+	}
+	
+	
+	public void displayHashTable() {
+		if(hashTable == null) {
+			System.out.println("Hash Table does not exists!");
+			return;
+		} else {
+			System.out.println("------Hash Table-------");
+			for(int i = 0; i < hashTable.length; i++) {
+				System.out.println("index " + i + ", key: " + hashTable[i]);
+			}
+		}
+	}
+}
--- hashing/Hashing/src/main/Main.java
+++ hashing/Hashing/src/main/Main.java
@@ -1,27 +1,35 @@
 package main;
 
 import hashing.DirectChaining;
+import hashing.LinearProbing;
 
 public class Main {
 	
 	public static void main(String[] args) {
-		DirectChaining dc = new DirectChaining(15);
-		dc.insertHashTable("The");
-		dc.insertHashTable("Quick");
-		dc.insertHashTable("Brown");
-		dc.insertHashTable("String");
-		dc.insertHashTable("Short");
-		dc.insertHashTable("quick");
-		dc.insertHashTable("fox");
+//		DirectChaining dc = new DirectChaining(15);
+//		dc.insertHashTable("The");
+//		dc.insertHashTable("Quick");
+//		dc.insertHashTable("Brown");
+//		dc.insertHashTable("String");
+//		dc.insertHashTable("Short");
+//		dc.insertHashTable("quick");
+//		dc.insertHashTable("fox");
+//		
+//		//dc.displayHashTable();
+//		dc.searchHashTable("fox");
+//		dc.deleteKeyHashTable("fox");
+//		dc.searchHashTable("fox");
 		
-		//dc.displayHashTable();
-		dc.searchHashTable("fox");
-		dc.deleteKeyHashTable("fox");
-		dc.searchHashTable("fox");
 		
-		
-	
-	
+	LinearProbing lp = new LinearProbing(13);
+	lp.insert("The");
+	lp.insert("Quick");
+	lp.insert("Brown");
+	lp.insert("String");
+	lp.insert("Short");
+	lp.insert("quick");
+	lp.insert("fox");
+	lp.displayHashTable();
 	
 	}
 	
